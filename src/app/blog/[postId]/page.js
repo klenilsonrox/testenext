8cca9eae39392028f4f947d33c69ab54a374309c
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-21 16:15:46 -0300

    update

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -9,7 +9,7 @@ const postJson = await reqPost.json()
     <div className='flex items-center w-screen h-screen justify-center p-2'>
     
      <div className='p-2 shadow-lg w-screen max-w-md flex flex-col gap-2 relative'>
-     <button className='absolute pb-2 right-0 top-[-35px]'><a href="/" className='text-red-600'>← voltar</a></button>
+     <button className='absolute pb-2 right-0 top-[-35px]'><a href="/blog" className='text-red-600'>← voltar</a></button>
      <h1 className='text-[23px]  leading-6 font-semibold'>{postJson.title}</h1>
      <p>{postJson.body}</p>
      </div>
